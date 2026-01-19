2eab75f7661a59fb2cd8d8bd50816f42472b2421
wingedcatgirl <69098783+wingedcatgirl@users.noreply.github.com>
2026-01-19 00:18:48 -0500

    +apple (why is dragonfruit doing what the doc says grapes will)

--- items/appetizers/apple.lua
+++ items/appetizers/apple.lua
@@ -1,23 +1,47 @@
--- SMODS.Joker {
---     key = "a_apple",
---     name = "Apple",
---     config = {
---         extra = {
-            
---         }
---     },
---     pos = { x = 2, y = 0 },
---     cost = 1,
---     rarity = 1,
---     blueprint_compat = true,
---     atlas = "joker",
---     loc_vars = function(self, info_queue, card)
---         return {
---             vars = {
-                
---             }
---         }
---     end,
---     calculate = function(self, card, context)
---     end
--- }
+ SMODS.Joker {
+     key = "a_apple",
+     name = "Apple",
+     config = {
+         extra = {
+            mult = 4,
+            drop = 1
+         }
+     },
+     pos = { x = 2, y = 0 },
+     cost = 1,
+     rarity = 1,
+     blueprint_compat = true,
+     atlas = "joker",
+     loc_vars = function(self, info_queue, card)
+         return {
+             vars = {
+                card.ability.extra.mult,
+                card.ability.extra.drop
+             }
+         }
+     end,
+     calculate = function(self, card, context)
+        if context.individual and context.cardarea == G.play then
+            SMODS.scale_card(card, {
+                ref_table = context.other_card,
+                ref_value = "perma_mult",
+                scalar_table = card.ability.extra,
+                scalar_value = "mult"
+            })
+        end
+
+        if context.end_of_round and context.main_eval then
+            SMODS.scale_card(card,{
+                ref_table = card.ability.extra,
+                ref_value = "mult",
+                scalar_value = "drop",
+                operation = "-"
+            })
+
+            if card.ability.extra.mult <= 0 then
+                SMODS.calculate_effect({message = localize("k_eaten_ex")}, card)
+                card:set_ability("j_bof_a_apple_core") --Do we want instant core, or Cavendish logic?
+            end
+        end
+     end
+ }
--- items/appetizers/apple_core.lua
+++ items/appetizers/apple_core.lua
@@ -1,27 +1,27 @@
--- SMODS.Joker {
---     key = "a_apple_core",
---     name = "Apple Core",
---     config = {
---         extra = {
+ SMODS.Joker {
+     key = "a_apple_core",
+     name = "Apple Core",
+     config = {
+         extra = {
             
---         }
---     },
---     pos = { x = 3, y = 0 },
---     cost = 1,
---     rarity = 1,
---     blueprint_compat = true,
---     atlas = "joker",
---     loc_vars = function(self, info_queue, card)
---         return {
---             vars = {
+         }
+     },
+     pos = { x = 3, y = 0 },
+     cost = 1,
+     rarity = 1,
+     blueprint_compat = true,
+     atlas = "joker",
+     loc_vars = function(self, info_queue, card)
+         return {
+             vars = {
                 
---             }
---         }
---     end,
---     calculate = function(self, card, context)
+             }
+         }
+     end,
+     calculate = function(self, card, context)
 
---     end,
---     in_pool = function(self, args)
---         return false
---     end
--- }
+     end,
+     in_pool = function(self, args)
+         return false
+     end
+ }
