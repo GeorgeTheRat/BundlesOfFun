6d853085793ff6ed0849aa08d52b6fe72a32bdba
GeorgeTheRat <earth.guy422@gmail.com>
2026-01-21 07:58:09 -0500

    i'm fixing the rarities

--- items/appetizers/dragonfruit.lua
+++ items/appetizers/dragonfruit.lua
@@ -8,7 +8,7 @@ SMODS.Joker {
     },
     pos = { x = 5, y = 0 },
     cost = 1,
-    rarity = 1,
+    rarity = 3,
     blueprint_compat = true,
     atlas = "joker",
     loc_vars = function(self, info_queue, card)
