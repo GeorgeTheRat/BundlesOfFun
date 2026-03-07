25aa2be512d13c84fc16ad5400f17d1872002a67
kierkat10 <kiernan.simpson@gmail.com>
2026-03-06 23:26:02 -0700

    oops

--- items/jesters/phony.lua
+++ items/jesters/phony.lua
@@ -24,33 +24,10 @@ SMODS.Joker {
         if context.joker_main then
             return {
                 mult = card.ability.extra.mult,
-                chips = -card.ability.extra.chips
+                extra = {
+                    chips = -card.ability.extra.chips
+                }
             }
         end
     end
-}
-
-SMODS.Joker {
-    key = "j_phony",
-    name = "Phony",
-    config = { extra = { mult = 6 } },
-    pos = { x = 7, y = 2 },
-    cost = 2,
-    rarity = 1,
-    blueprint_compat = true,
-    atlas = "joker",
-    loc_vars = function(self, info_queue, card)
-        return { vars = { card.ability.extra.mult } }
-    end,
-    calculate = function(self, card, context)
-        if context.joker_main then
-            return {
-                mult = card.ability.extra.mult
-            }
-        end
-    end
-}
-
-return {
-    
 }
