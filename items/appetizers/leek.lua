013817cded7c11696c2ac4c1f386f6f43fc5dcce
= <devrimulasyurtsever00@gmail.com>
2026-03-30 20:08:17 +0300

    leek get eaten

--- items/appetizers/leek.lua
+++ items/appetizers/leek.lua
@@ -22,24 +22,31 @@ SMODS.Joker {
     calculate = function(self, card, context)
         local cae = card.ability.extra
         if context.mod_probability and not context.blueprint then
+            card.ability.extra.probabilities_current = card.ability.extra.probabilities_current or card.ability.extra.probabilities_start
             return {
                 numerator = context.numerator + cae.probabilities_current
             }
         end
         if context.pseudorandom_result and context.result then
-            if not card.ability.extra.probabilities_current then --ignore pls
-                card.ability.extra.probabilities_current = card.ability.extra.probabilities_start 
-            end
-            SMODS.scale_card(card, {
-                ref_table = cae,
-                ref_value = "probabilities_current",
-                scalar_value = "decrease",
-                operation = "-",
-                scaling_message = {
-                    message = localize("k_bof_downgrade"),
-                    colour = G.C.RED
-                }
-            })
+           card.ability.extra.probabilities_current = card.ability.extra.probabilities_current or card.ability.extra.probabilities_start
+            if cae.probabilities_current >= (cae.decrease) then
+                SMODS.scale_card(card, {
+                    ref_table = cae,
+                    ref_value = "probabilities_current",
+                    scalar_value = "decrease",
+                    operation = "-",
+                    scaling_message = {
+                        message = localize("k_bof_downgrade"),
+                        colour = G.C.RED
+                    }
+                })
+                else
+                    SMODS.destroy_cards(card, nil, nil, true)
+                    return{
+                        message = localize('k_eaten_ex'),
+                    }
+                end
+         
         end
     end
 }
