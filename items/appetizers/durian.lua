338fe19f04e02138f992b5a814cc75e62c50a23d
kierkat10 <kiernan.simpson@gmail.com>
2026-06-01 00:41:01 -0700

    ...and durian

--- items/appetizers/durian.lua
+++ items/appetizers/durian.lua
@@ -12,7 +12,7 @@ SMODS.Joker {
     end,
     calculate = function(self, card, context)
         if context.selling_self then
-            for i = 1, G.consumeables.config.card_limit - #G.consumeables.cards do
+            for i = 1, math.min(100, G.consumeables.config.card_limit - #G.consumeables.cards) do
                 G.GAME.consumeable_buffer = G.GAME.consumeable_buffer + 1
                 G.E_MANAGER:add_event(Event({
                     func = (function()
