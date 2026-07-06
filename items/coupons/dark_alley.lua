d042c68c107f33742955a317ad55656704be91c7
kierkat10 <kiernan.simpson@gmail.com>
2026-07-05 22:22:07 -0700

    oops

--- items/coupons/dark_alley.lua
+++ items/coupons/dark_alley.lua
@@ -2,15 +2,10 @@ BundlesOfFun.Voucher {
     key = "dark_alley",
     name = "Dark Alley",
     bundle = "coupons",
-    config = { spectral_rate = 69 },
+    config = { spectral_rate = 1 },
     pos = { x = 0, y = 0 },
     atlas = "voucher",
     redeem = function(self, card, area)
-        G.E_MANAGER:add_event(Event({
-            func = function()
-                G.GAME.spectral_rate = self.config.spectral_rate
-                return true
-            end
-        }))
+        G.GAME.spectral_rate = self.config.spectral_rate
     end
 }
