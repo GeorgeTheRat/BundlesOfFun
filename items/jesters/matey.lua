2f523d69af8b5f2e6ad64b19e6d84dfc1cfa0e0c
kierkat10 <kiernan.simpson@gmail.com>
2026-07-20 11:59:18 -0700

    make matey alt not appear in collection

--- items/jesters/matey.lua
+++ items/jesters/matey.lua
@@ -9,10 +9,15 @@ BundlesOfFun.Joker {
     blueprint_compat = false,
     atlas = "joker",
     set_ability = function(self, card, initial, delay_sprites)
-		if self.discovered and not (card.area and card.area.config.collection) then
-			if pseudorandom("bof_matey") > 0.9 then
-				card.children.center:set_sprite_pos({ x = 1, y = 6 })
-			end
-		end
+        G.E_MANAGER:add_event(Event({
+            func = function()
+                if self.discovered and not (card.area and card.area.config.collection) then
+                    if pseudorandom("bof_matey") > 0.9 then
+                        card.children.center:set_sprite_pos({ x = 1, y = 6 })
+                    end
+                end
+                return true
+            end
+        }))
 	end
 }
