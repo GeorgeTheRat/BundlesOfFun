cbdac4a272290cb2c7836730b5656d6315e19044
kierkat10 <kiernan.simpson@gmail.com>
2026-07-30 19:29:10 -0700

    card-en gnome fixes

--- items/normalities/gnome.lua
+++ items/normalities/gnome.lua
@@ -9,14 +9,32 @@ BundlesOfFun.Joker {
     blueprint_compat = false,
     atlas = "joker",
     calculate = function(self, card, context)
+        if context.modify_shop_card then
+            G.E_MANAGER:add_event(Event({
+                func = function()
+                    if context.card.edition then
+                        context.card.ability.couponed = true
+                        context.card:set_cost()
+                    end
+                    return true
+                end
+            }))
+        end
         if
             context.post_trigger and
             context.other_card and
-            context.other_card.config.center and
             (
-                context.other_card.config.center.key == "v_bof_dark_alley" or
-                context.other_card.config.center.key == "b_bof_fossilized" or
-                context.other_card.config.center.key == "v_bof_illegal_wares"
+                (
+                    context.other_card.config.center and
+                    (
+                        context.other_card.config.center.key == "v_bof_dark_alley" or
+                        context.other_card.config.center.key == "v_bof_illegal_wares"
+                    )
+                ) or (
+                    context.other_card.effect and
+                    context.other_card.effect.center and
+                    context.other_card.effect.center.key == "b_bof_fossilized"
+                )
             )
         then
             G.E_MANAGER:add_event(Event({
