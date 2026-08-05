ae121cf1fc61dece96555f22938cfce1b9671d8a
kierkat10 <kiernan.simpson@gmail.com>
2026-08-04 21:17:05 -0700

    larry $5 -> $6

--- items/jesters/larry.lua
+++ items/jesters/larry.lua
@@ -5,7 +5,7 @@ BundlesOfFun.Joker {
     config = { extra = { dollars = 3 } },
     pos = { x = 9, y = 3 },
     attributes = { "economy", "hands" },
-    cost = 5,
+    cost = 6,
     rarity = 1,
     blueprint_compat = true,
     atlas = "joker",
