2a90e54cec1bb1ad4896c62314822ee60e819790
kierkat10 <kiernan.simpson@gmail.com>
2026-08-22 18:32:59 -0700

    hypnotic jokerdisplay and very slight change

--- items/jesters/hypnotic.lua
+++ items/jesters/hypnotic.lua
@@ -26,20 +26,21 @@ BundlesOfFun.Joker {
     calculate = function(self, card, context)
         local most_played = 0
         local most_played_hand = "High Card"
+        local most_played_count = math.huge
         for hand, data in pairs(G.GAME.hands) do
+            local true_count = 0
+            for _, hand_card in ipairs(data.example) do
+                if hand_card[2] == true then
+                    true_count = true_count + 1
+                end
+            end
             if data.played > most_played then
                 most_played = data.played
                 most_played_hand = hand
-            elseif data.played == most_played then
-                local tied_true_count = 0
-                for _, hand_card in ipairs(data.example) do
-                    if hand_card[2] == true then
-                        tied_true_count = tied_true_count + 1
-                    end
-                end
-                if tied_true_count < card.ability.extra.count then
-                    most_played_hand = hand
-                end
+                most_played_count = true_count
+            elseif data.played == most_played and true_count < most_played_count then
+                most_played_hand = hand
+                most_played_count = true_count
             end
         end
         local current_has_five = false
@@ -66,5 +67,47 @@ BundlesOfFun.Joker {
                 card_eval_status_text(card, "extra", nil, nil, nil, { message = localize({ type = "variable", key = "a_handsize_minus", vars = { card.ability.extra.hand_size } }) })
             end
         end
+    end,
+    joker_display_def = function(JokerDisplay)
+        return {
+            text = {
+                { text = "+" },
+                { ref_table = "card.joker_display_values", ref_value = "hand_size" },
+            },
+            text_config = { colour = G.C.FILTER },
+            reminder_text = {
+                { text = "(" },
+                { ref_table = "card.joker_display_values", ref_value = "most_played_hand" },
+                { text = ")" },
+            },
+            calc_function = function(card)
+                local most_played = 0
+                local most_played_hand = "High Card"
+                local most_played_count = math.huge
+                for hand, data in pairs(G.GAME.hands) do
+                    local true_count = 0
+                    if data.example then
+                        for _, hand_card in ipairs(data.example) do
+                            if hand_card[2] == true then
+                                true_count = true_count + 1
+                            end
+                        end
+                    end
+                    if data.played > most_played then
+                        most_played = data.played
+                        most_played_hand = hand
+                        most_played_count = true_count
+                    elseif data.played == most_played and true_count < most_played_count then
+                        most_played_hand = hand
+                        most_played_count = true_count
+                    end
+                end
+                card.joker_display_values.hand_size =
+                    most_played_count == card.ability.extra.count
+                    and card.ability.extra.hand_size
+                    or 0
+                card.joker_display_values.most_played_hand = most_played_hand
+            end
+        }
     end
 }
