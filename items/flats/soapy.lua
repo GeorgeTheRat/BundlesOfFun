5ceb156f085d164bae12abb3728b28f5172f7064
Glitchkat10 <136650292+kierkat10@users.noreply.github.com>
2026-09-17 11:07:40 -0700

    soapy no destroy debuff

--- items/flats/soapy.lua
+++ items/flats/soapy.lua
@@ -6,12 +6,10 @@ BundlesOfFun.Back {
 	pos = { x = 6, y = 0 },
     unlocked = false,
     calculate = function(self, back, context)
-        if context.discard then
-            if next(SMODS.get_enhancements(context.other_card)) ~= nil then
-                return {
-                    remove = true
-                }
-            end
+        if context.discard and not context.other_card.debuff and next(SMODS.get_enhancements(context.other_card)) ~= nil then
+            return {
+                remove = true
+            }
         end
     end,
     check_for_unlock = function(self, args)
